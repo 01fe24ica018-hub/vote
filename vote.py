5b375b9ba3a3e7fa1f0f0b8a5e0e93c21600ac3c
sanjana <01fe24ica018@kletech.ac.in>
2025-10-17 09:42:37 +0530

    voting eligiblity

---
+++ vote.py
@@ -0,0 +1,12 @@
+def check_voter_eligibility():
+    try:
+        age = int(input("Enter your age: "))
+        if age >= 18:
+            print("You are eligible to vote.")
+        else:
+            print("You are not eligible to vote.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number for age.")
+
+
+check_voter_eligibility()
